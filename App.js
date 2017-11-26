630f0295e6849c3b4e7d562ae39fe05c874255b4
kazuya nakajima <uniss010@gmail.com>
2017-11-26 14:46:57 +0900

    install react-redux

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { Constants } from 'expo'
+import { createStore } from 'react'
+import { Provider } from 'react-redux'
 import { udacityBlue } from './utils/colors'
 import { Navigator } from './components/Navigator'
 
@@ -10,13 +12,20 @@ const MyStatusBar = ({ backgroundColor, ...props }) => (
   </View>  
 )
 
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <MyStatusBar backgroundColor={udacityBlue} barStyle='light-content'/>
-        <Navigator />
-      </View>
+      <Provider store={store}>
+        <View style={{flex: 1}}>
+          <MyStatusBar backgroundColor={udacityBlue} barStyle='light-content'/>
+          <Navigator />
+        </View>        
+      </Provider>
     );
   }
 }
